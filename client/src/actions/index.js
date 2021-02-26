f22081ebd637acbb8a99f224406d2a598c19adca
electromidz <electromidz@gmail.com>
2021-02-26 23:23:10 +0330

    add somethings

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,6 @@
 import {SIGN_IN , SIGN_OUT} from './types';
+import streams from '../apis/streams';
+
 export const signIn = (userId) => {
     return {
         type: SIGN_IN,
@@ -9,5 +11,12 @@ export const signIn = (userId) => {
 export const signOut = () => {
     return {
         type: SIGN_OUT,
+    };
+};
+
+
+export const createStream = (formValues) => {
+    return async (dispatch) => {
+        streams.post('/streams', formValues);
     };
 };
--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Field , reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import { createStream } from '../../actions';
 
 class StreamCreate extends Component {
 
@@ -26,8 +28,8 @@ renderInput = ({input , lable , meta}) => {
     );
 };
 
-onSubmit(formValues) {
-    console.log(formValues);
+onSubmit = (formValues) => {
+    this.props.createStream(formValues);
 }
 
     render() {
@@ -55,7 +57,9 @@ const validate = formValues => {
     return errors;
 };
 
-export default reduxForm({
+const formWrapped =  reduxForm({
     form: 'streamCreate',
     validate : validate,
 })(StreamCreate); 
+
+export default connect(null, { createStream })(formWrapped);
