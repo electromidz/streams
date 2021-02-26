7d1e2afae2be93b204a2d8dc05881438d63c7686
electromidz <electromidz@gmail.com>
2021-02-27 00:27:10 +0330

    CRUD BULK

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,4 +1,12 @@
-import {SIGN_IN , SIGN_OUT} from './types';
+import {SIGN_IN, 
+    SIGN_OUT, 
+    CREATE_STREAM,
+    FETCH_STREAMS,
+    FETCH_STREAM,
+    EDIT_STREAM,
+    DELETE_STREAM,
+} from './types';
+
 import streams from '../apis/streams';
 
 export const signIn = (userId) => {
@@ -17,6 +25,31 @@ export const signOut = () => {
 
 export const createStream = (formValues) => {
     return async (dispatch) => {
-        streams.post('/streams', formValues);
+        const response = await streams.post('/streams', formValues);
+        dispatch({
+            type:CREATE_STREAM,
+            payload: response.data,
+        });
     };
+};
+
+export const fetchStreams = () => async dispatch => {
+    const response = await streams.get('/streams');
+    dispatch({ type: FETCH_STREAMS, payload: response.data});
+};
+
+export const fetchStream = (id) => async dispatch => {
+    const response = await streams.get(`/streams/${id}`);
+    dispatch({type: FETCH_STREAM, payload: response.data});
+};
+
+export const editStream = (id, formValues) => async dispatch => {
+    const response = await streams.put(`/streams/${id}`, formValues);
+    dispatch({type: EDIT_STREAM, payload: response.data});
+};
+
+export const deleteStream = (id) => async dispatch => {
+    //Nothing to return so we dont`t need response
+    await streams.delete(`/streams/${id}`);
+    dispatch({type: DELETE_STREAM, payload: id});
 };
