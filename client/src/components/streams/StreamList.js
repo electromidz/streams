0efdb534a256b2fca3229d6563a8fc59e2c2c9ed
electromidz <electromidz@gmail.com>
2021-03-10 23:22:41 +0330

    do some code

--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,45 +1,72 @@
-import { fromPairs } from 'lodash';
-import React from 'react'
-import { connect } from 'react-redux';
-import { fetchStreams } from '../../actions';
+import { fromPairs } from "lodash";
+import React from "react";
+import { connect } from "react-redux";
+import { fetchStreams } from "../../actions";
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
-componentDidMount(){
+  componentDidMount() {
     this.props.fetchStreams();
-}
+  }
 
-renderList() {
-    console.log('streams');
+  renderAdmin(stream) {
+    if (stream.userId === this.props.currentUserId) {
+      return (
+        <div className="right floated content">
+          <button className="ui button primary">Edit</button>
+          <button className="ui button negative">Delete</button>
+        </div>
+      );
+    }
+  }
+
+  renderList() {
     return this.props.streams.map((stream) => {
-        return(
-            <div className="item" key={stream.id}>
-                <i className="large middle aligned icon camera" />
-                <div className="content">
-                    {stream.title}
-                    <div className="description">
-                        {stream.description}
-                    </div>
-                </div>
-            </div>
-        );
+      return (
+        <div className="item" key={stream.id}>
+          {this.renderAdmin(stream)}
+          <i className="large middle aligned icon camera" />
+          <div className="content">
+            {stream.title}
+            <div className="description">{stream.description}</div>
+          </div>
+        </div>
+      );
     });
-};
+  }
 
-    render() {
-        // console.log(this.props.streams);
-        return (
-            <div>
-                <h2>Streams</h2>
-                <div className="ui called list">
-                    {this.renderList()}
-                </div>
-            </div>
-        );
-    };
-};
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{textAlign: 'right'}}>
+          <Link to="/stream/new" className="ui button green">
+            Create Stream
+          </Link>
+        </div>
+      );      
+    }
+
+  }
+  render() {
+    // console.log(this.props.streams);
+    return (
+      <div>
+        <h2>Streams</h2>
+        <div className="ui celled list">
+          {this.renderList()}
+        </div>
+        {this.renderCreate()}
+      </div>
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
-    return {streams: Object.values(state.streams)};
-}
+  return {
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId,
+    isSignedIn : state.auth.isSignedIn,
+  };
+};
 
-export default connect(mapStateToProps , {fetchStreams})(StreamList);
+export default connect(mapStateToProps, { fetchStreams })(StreamList);
--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -1,26 +1,25 @@
-import _ from 'lodash';
+import _ from "lodash";
 import {
-        CREATE_STREAM,
-        FETCH_STREAMS,
-        FETCH_STREAM,
-        EDIT_STREAM,
-        DELETE_STREAM,
-} from '../actions/types';
+  CREATE_STREAM,
+  FETCH_STREAMS,
+  FETCH_STREAM,
+  EDIT_STREAM,
+  DELETE_STREAM,
+} from "../actions/types";
 
-
-export default (state= {}, action) => {
-    switch (action.type) {
-        case FETCH_STREAMS:
-            return {...state, ..._.mapKeys(action.payload, 'id') };
-        case FETCH_STREAM:
-            return {...state, [action.payload.id]: action.payload}
-        case CREATE_STREAM:
-            return {...state, [action.payload.id]: action.payload}
-        case EDIT_STREAM:
-            return {...state, [action.payload.id]: action.payload}
-        case DELETE_STREAM:
-            return _.omit(state, action.id);
-        default:
-        return state;
-    }
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.id);
+    default:
+      return state;
+  }
 };
